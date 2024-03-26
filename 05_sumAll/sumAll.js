8497d8a667b357518ee1648b5081c7975e4d7c0a
André Louro <80702301+Arlouro@users.noreply.github.com>
2024-03-26 17:31:32 +0000

    Solved exercise 5 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
 const sumAll = function() {
+  let start = arguments[0];
+  let end = arguments[1];
+  let sum = 0;
 
+  if (typeof start !== 'number' || typeof end !== 'number') {
+    return 'ERROR';
+  }
+
+  if (start < 0 || end < 0) {
+    return 'ERROR';
+  }
+
+  for (let i = Math.min(start, end); i <= Math.max(start, end); i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
