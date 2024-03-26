bd45e50ce8d55bd73efb54742ba6eacc33f0183d
André Louro <80702301+Arlouro@users.noreply.github.com>
2024-03-26 17:10:05 +0000

    Solved exercise 2 repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, repeatAmount) {
+  if (repeatAmount < 0) {
+    return 'ERROR';
+  }
 
+  let concatenatedString = '';
+  for (let i = 0; i < repeatAmount; i++) {
+    concatenatedString += string;
+  }
+  return concatenatedString;
 };
 
 // Do not edit below this line
