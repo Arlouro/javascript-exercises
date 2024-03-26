90c9c262d7197663c8e583277e5f8f1246c099d7
André Louro <80702301+Arlouro@users.noreply.github.com>
2024-03-26 18:11:56 +0000

    Solved exercise 6 leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
 const leapYears = function() {
+  let year = arguments[0];
 
+  if (year % 4 === 0 && year % 100 !== 0) {
+    return true;
+  } else if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
